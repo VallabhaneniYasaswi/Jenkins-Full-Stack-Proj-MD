af639c22a0a2029a2c8a7844454698403aa14fa4
Yasaswi <yasaswivallabhaneni11@gmail.com>
2025-08-17 18:23:05 +0530

    Updated servletInitializer

--- MD-SPRINGBOOT/src/main/java/com/klef/MdSpringbootApplication.java
+++ MD-SPRINGBOOT/src/main/java/com/klef/MdSpringbootApplication.java
@@ -2,12 +2,14 @@ package com.klef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MdSpringbootApplication {
+public class MdSpringbootApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MdSpringbootApplication.class, args);
+		System.out.println("Project is Running ...");
 	}
 
 }
