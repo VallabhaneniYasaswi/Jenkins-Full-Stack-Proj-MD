6e231e6e87fbaf29be24f8b4a92e783bf0d54d71
Yasaswi <yasaswivallabhaneni11@gmail.com>
2025-08-17 21:11:05 +0530

    updated springbootapplication

--- MD-SPRINGBOOT/src/main/java/com/klef/MdSpringbootApplication.java
+++ MD-SPRINGBOOT/src/main/java/com/klef/MdSpringbootApplication.java
@@ -2,15 +2,19 @@ package com.klef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MdSpringbootApplication extends SpringBootServletInitializer{
-	
+public class MdSpringbootApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MdSpringbootApplication.class, args);
-		System.out.println("Project is Running ...");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MdSpringbootApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(MdSpringbootApplication.class, args);
+        System.out.println("Project is Running ...");
+    }
 }
